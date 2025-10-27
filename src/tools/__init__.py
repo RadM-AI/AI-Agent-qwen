2c1c4a63116d7d2666cdb02be057364e0b728dd7
Radmir Mannanov <radmir.mannanov@bk.ru>
2025-10-27 14:12:19 +0000

    update

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -5,13 +5,13 @@
 from .base import BaseTool
 from .calculator import CalculatorTool
 from .weather import WeatherTool
-# from .search import SearchTool
+from .search import SearchTool
 from .registry import ToolRegistry
 
 __all__ = [
     "BaseTool",
     "CalculatorTool",
     "WeatherTool", 
-    # "SearchTool",
+    "SearchTool",
     "ToolRegistry",
 ]
